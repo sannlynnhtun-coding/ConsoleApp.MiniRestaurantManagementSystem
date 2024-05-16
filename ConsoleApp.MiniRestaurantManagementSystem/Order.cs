8feda64357dda416952820067936f7071e576269
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-16 23:17:44 +0630

    [Complete] Order

---
+++ ConsoleApp.MiniRestaurantManagementSystem/Order.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp.MiniRestaurantManagementSystem;
+
+class Order
+{
+    public int Id { get; set; }
+    public List<OrderItem> Items { get; set; }
+
+    public Order()
+    {
+        Items = new List<OrderItem>();
+    }
+
+    public decimal GetTotal()
+    {
+        decimal total = 0;
+        foreach (OrderItem item in Items)
+        {
+            total += item.GetTotal();
+        }
+        return total;
+    }
+}
