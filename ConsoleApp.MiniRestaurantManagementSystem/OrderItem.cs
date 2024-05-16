15bc525ae77349766340fc12933aa3d53599d503
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-16 23:17:54 +0630

    [Complete] OrderItem

---
+++ ConsoleApp.MiniRestaurantManagementSystem/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp.MiniRestaurantManagementSystem;
+
+class OrderItem
+{
+    public MenuItem Item { get; set; }
+    public int Quantity { get; set; }
+
+    public decimal GetTotal()
+    {
+        return Item.Price * Quantity;
+    }
+}
