033c8fbf1fdf3cf43c267aa8c05715d9336f646b
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-16 23:17:32 +0630

    [Complete] MenuItem

---
+++ ConsoleApp.MiniRestaurantManagementSystem/MenuItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp.MiniRestaurantManagementSystem;
+
+class MenuItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
